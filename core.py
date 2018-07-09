f2b33bd47da7209afad2566b3a8d993ebefcb794
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-07-09 16:18:04 -0500

    added tests.

--- core.py
+++ core.py
@@ -12,17 +12,15 @@ def new_gladiator(health, rage, damage_low, damage_high):
 
 
 def attack(attacker, defender):
-    damage_dealt = randint(damage_low, damage_high)
+    damage_dealt = randint(attacker['damage_low'], attacker['damage_high'])
     critical_hit = damage_dealt * 2
     normal_hit = damage_dealt
     if randint(1, 100) < attacker['rage']:
         attacker['rage'] = 0
         defender['health'] = defender['health'] - critical_hit
-        return defender['health'], attacker
     else:
         attacker['rage'] = attacker['rage'] + 15
-         defender['health'] = defender['health'] - normal_hit
-        return defender['health']
+        defender['health'] = defender['health'] - normal_hit
 
 
 def heal(gladiator):
@@ -30,6 +28,7 @@ def heal(gladiator):
         print('You cannot heal if your health is at the max level.')
     else:
         gladiator['rage'] = gladiator['rage'] - 10
+        gladiator['health'] = gladiator['health'] + 5
         return gladiator
 
 
