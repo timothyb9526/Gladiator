21b50cc8df8f45fb8995368188c291cd79090414
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-07-10 13:40:08 -0500

    added a power meter

--- core.py
+++ core.py
@@ -1,19 +1,16 @@
 from random import randint
+from shell import *
 
 
-def new_gladiator(health, rage, damage_low, damage_high):
+def new_gladiator(health, rage, damage_low, damage_high, power):
     gladiator = {
         'health': health,
         'rage': rage,
         'damage_low': damage_low,
-        'damage_high': damage_high
+        'damage_high': damage_high,
+        'power': power
     }
     return gladiator
-
-
-def new_weapons(attack_dmage):
-
-    weapons = {'Bow':'Attack damage': 20}, {'sword': 'Attack damage': 30}
 
 
 def attack(attacker, defender):
@@ -23,14 +20,28 @@ def attack(attacker, defender):
     if randint(1, 100) < attacker['rage']:
         attacker['rage'] = 0
         defender['health'] = defender['health'] - critical_hit
+        attacker['power'] = attacker['power'] + 40
+
     else:
         attacker['rage'] = attacker['rage'] + 15
+        attacker['power'] = attacker['power'] + 20
         defender['health'] = defender['health'] - normal_hit
+
+    if attacker['power'] == 400:
+        defender['health'] = defender['health'] - defender['health']
 
 
 def heal(gladiator):
     if gladiator['health'] == 100:
         print('You cannot heal if your health is at the max level.')
+
+    elif gladiator['health'] <= 100:
+        gladiator['power'] = gladiator['power'] - 35
+        gladiator['health'] = gladiator['health'] + 30
+
+    elif gladiator['power'] >= 260 and gladiator['health'] <= 150:
+        gladiator['power'] = gladiator['power'] - gladiator['power']
+        gladiator['health'] = gladiator['health'] + 100
     else:
         gladiator['rage'] = gladiator['rage'] - 10
         gladiator['health'] = gladiator['health'] + 5
--- shell.py
+++ shell.py
@@ -1,20 +1,21 @@
 from core import *
 
 
-def gladiator(gladiator1, gladiator2, weapons):
+def gladiator(gladiator1, gladiator2):
     while True:
 
         attack_method = input(
             'gladiator1 .... What would you like to do?\n- [A]ttack\n- [P]ass\n- [Q]uit\n- [H]eal\n'
         )
-        weapon_choice = input('GLADIATOR1 CHOOSE YOUR WEAPON!!\n')
         if attack_method == 'a':
 
             attack(gladiator1, gladiator2)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
-                  gladiator1['rage'], 'Rage')
+                  gladiator1['rage'], 'Rage', '|||', gladiator1['power'],
+                  'power')
             print('gladiator2', gladiator2['health'], 'HP', '|||',
-                  gladiator2['rage'], 'Rage')
+                  gladiator2['rage'], 'Rage', '|||', gladiator2['power'],
+                  'power')
             break
         elif attack_method == 'q':
             print('Gladiator1: SURVIVED!!!\nGladiator2: SURVIVED!!!')
@@ -25,9 +26,11 @@ def gladiator(gladiator1, gladiator2, weapons):
         elif attack_method == 'h':
             heal(gladiator1)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
-                  gladiator1['rage'], 'Rage')
+                  gladiator1['rage'], 'Rage', '|||', gladiator1['power'],
+                  'power')
             print('gladiator2', gladiator2['health'], 'HP', '|||',
-                  gladiator2['rage'], 'Rage')
+                  gladiator2['rage'], 'Rage', '|||', gladiator2['power'],
+                  'power')
             break
 
 
@@ -41,9 +44,11 @@ def gladiator2(gladiator1, gladiator2):
 
             attack(gladiator2, gladiator1)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
-                  gladiator1['rage'], 'Rage')
+                  gladiator1['rage'], 'Rage', '|||', gladiator1['power'],
+                  'power')
             print('gladiator2', gladiator2['health'], 'HP', '|||',
-                  gladiator2['rage'], 'Rage')
+                  gladiator2['rage'], 'Rage', '|||', gladiator2['power'],
+                  'power')
             break
         elif gladiator_2 == 'q':
             print('Gladiator1: SURVIVED!!!\nGladiator2: SURVIVED!!!')
@@ -53,26 +58,33 @@ def gladiator2(gladiator1, gladiator2):
             print('Gladiator2 did nothing this round.')
             break
         elif gladiator_2 == 'h':
-            heal(gladiator1)
+            heal(gladiator2)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
-                  gladiator1['rage'], 'Rage')
+                  gladiator1['rage'], 'Rage', '|||', gladiator1['power'],
+                  'power')
             print('gladiator2', gladiator2['health'], 'HP', '|||',
-                  gladiator2['rage'], 'Rage')
+                  gladiator2['rage'], 'Rage', '|||', gladiator2['power'],
+                  'power')
             break
 
 
-
-def weapon():
-
-
 def battle():
-    g1 = {'health': 100, 'rage': 0, 'damage_low': 10, 'damage_high': 20}
+    g1 = {
+        'health': 300,
+        'rage': 0,
+        'damage_low': 10,
+        'damage_high': 20,
+        'power': 0
+    }
 
-    g2 = {'health': 100, 'rage': 0, 'damage_low': 10, 'damage_high': 20}
+    g2 = {
+        'health': 300,
+        'rage': 0,
+        'damage_low': 10,
+        'damage_high': 20,
+        'power': 0
+    }
 
-    weapons = {'Bow': 'Attack damage': 20}, {'sword': 'Attack damage': 30}
-    weapon(weapons)
-    
     while True:
         if is_dead(g1) == True:
             print('GLADIATOR2: DID NOT SURVIVE!!!')
@@ -80,7 +92,7 @@ def battle():
             break
             exit()
 
-        gladiator(g1, g2, weapons)
+        gladiator(g1, g2)
 
         if is_dead(g2) == True:
             print('GLADIATOR1: DID NOT SURVIVE!!!')
