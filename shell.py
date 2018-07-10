63c36d2bb82dabd6a4713bf10cf484340f7adae6
timothyb9526 <tbowling@basecampcodingacademy.org>
2018-07-10 11:57:29 -0500

    started to add weapons

--- shell.py
+++ shell.py
@@ -1,14 +1,14 @@
 from core import *
 
 
-def gladiator(gladiator1, gladiator2):
+def gladiator(gladiator1, gladiator2, weapons):
     while True:
 
         attack_method = input(
-            'gladiator1 .... What would you like to do?\n- attack\n- pass\n- quit\n- heal\n'
+            'gladiator1 .... What would you like to do?\n- [A]ttack\n- [P]ass\n- [Q]uit\n- [H]eal\n'
         )
-
-        if attack_method == 'attack':
+        weapon_choice = input('GLADIATOR1 CHOOSE YOUR WEAPON!!\n')
+        if attack_method == 'a':
 
             attack(gladiator1, gladiator2)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
@@ -16,13 +16,13 @@ def gladiator(gladiator1, gladiator2):
             print('gladiator2', gladiator2['health'], 'HP', '|||',
                   gladiator2['rage'], 'Rage')
             break
-        elif attack_method == 'quit':
+        elif attack_method == 'q':
             print('Gladiator1: SURVIVED!!!\nGladiator2: SURVIVED!!!')
             exit()
-        elif attack_method == 'pass':
+        elif attack_method == 'p':
             print('Gladiator1 did nothing this round.')
             break
-        elif attack_method == 'heal':
+        elif attack_method == 'h':
             heal(gladiator1)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
                   gladiator1['rage'], 'Rage')
@@ -34,10 +34,10 @@ def gladiator(gladiator1, gladiator2):
 def gladiator2(gladiator1, gladiator2):
     while True:
         gladiator_2 = input(
-            'gladiator2 .... What would you like to do?\n- attack\n- pass\n- quit\n- heal\n'
+            'gladiator2 .... What would you like to do?\n- [A]ttack\n- [P]ass\n- [Q]uit\n- [H]eal\n'
         )
 
-        if gladiator_2 == 'attack':
+        if gladiator_2 == 'a':
 
             attack(gladiator2, gladiator1)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
@@ -45,14 +45,14 @@ def gladiator2(gladiator1, gladiator2):
             print('gladiator2', gladiator2['health'], 'HP', '|||',
                   gladiator2['rage'], 'Rage')
             break
-        elif gladiator_2 == 'quit':
+        elif gladiator_2 == 'q':
             print('Gladiator1: SURVIVED!!!\nGladiator2: SURVIVED!!!')
 
             exit()
-        elif gladiator_2 == 'pass':
-            print('Gladiator1 did nothing this round.')
+        elif gladiator_2 == 'p':
+            print('Gladiator2 did nothing this round.')
             break
-        elif gladiator_2 == 'heal':
+        elif gladiator_2 == 'h':
             heal(gladiator1)
             print('gladiator1', gladiator1['health'], 'HP', '|||',
                   gladiator1['rage'], 'Rage')
@@ -61,13 +61,33 @@ def gladiator2(gladiator1, gladiator2):
             break
 
 
+
+def weapon():
+
+
 def battle():
     g1 = {'health': 100, 'rage': 0, 'damage_low': 10, 'damage_high': 20}
 
     g2 = {'health': 100, 'rage': 0, 'damage_low': 10, 'damage_high': 20}
 
+    weapons = {'Bow': 'Attack damage': 20}, {'sword': 'Attack damage': 30}
+    weapon(weapons)
+    
     while True:
-        gladiator(g1, g2)
+        if is_dead(g1) == True:
+            print('GLADIATOR2: DID NOT SURVIVE!!!')
+            print('GLADIATOR1: WINS!!!')
+            break
+            exit()
+
+        gladiator(g1, g2, weapons)
+
+        if is_dead(g2) == True:
+            print('GLADIATOR1: DID NOT SURVIVE!!!')
+            print('GLADIATOR2: WINS!!!')
+            break
+            exit()
+
         gladiator2(g1, g2)
 
 
